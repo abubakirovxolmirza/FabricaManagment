e58d1afcdab9317892a3e7e7c01a16352b5bdc17
abubakirovxolmirza <137431926+abubakirovxolmirza@users.noreply.github.com>
2024-03-19 08:33:00 +0500

    Update urls.py

--- product/urls.py
+++ product/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import ProductList, MaterialList, WareHouseList, ProductMaterialList, add_products
 
 urlpatterns = [
-    path('products/', ProductList.as_view()),
-    path('warehouse/', WareHouseList.as_view()),
-    path('material/', MaterialList.as_view()),
-    path('product-material/', ProductMaterialList.as_view()),
-    path('add/', add_products),
+    path('products/', ProductList.as_view(), name='products'),
+    path('warehouse/', WareHouseList.as_view(), name='warehouse'),
+    path('material/', MaterialList.as_view(), name='material'),
+    path('product-material/', ProductMaterialList.as_view(), name='product-material'),
+    path('add/', add_products, name='add_products'),
 ]
